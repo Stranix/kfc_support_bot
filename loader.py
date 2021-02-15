a0ee34a9875ef532d1e9cd584fc45a167c9caeac
Stranix <Phantom2525@gmail.com>
2021-02-15 15:33:12 +0300

    измнение параметров логгирования

--- loader.py
+++ loader.py
@@ -7,7 +7,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import env_config as config
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-logging.basicConfig(level=logging.INFO)
+FORMAT = '%(asctime)s %(levelname)s : %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p')
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
