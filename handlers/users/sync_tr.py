1983059006caf5c794e694fb75912c526680eb1f
konst <stranix05@inbox.ru>
2021-03-24 10:21:43 +0300

    обозначение в чате какой транзит выбрали

--- handlers/users/sync_tr.py
+++ handlers/users/sync_tr.py
@@ -20,6 +20,11 @@ async def choice_tr_group(message: types.Message):
 async def kb_answer_for_sync_tr(query: types.CallbackQuery):
     await query.answer()
     logging.info(f'Получен ответ: {query.data}')
+    tranzit_name = ''
+    if query.data == 'yum':
+        tranzit_name = 'ЯМ'
+    if query.data == 'irb':
+        tranzit_name = 'ИРБ'
 
     if query.data == 'all':
         links_to_sync = generated_links_to_sync(['yum', 'irb'])
@@ -36,7 +41,7 @@ async def kb_answer_for_sync_tr(query: types.CallbackQuery):
             sync_error += f'\nTranzit: {result["web_link"]}\nStatus: {result["status"]}\n'
 
     if sync_error == '':
-        message_for_send = 'Запуск синхронизации по транзитам: Успех'
+        message_for_send = f'Запуск синхронизации по транзитам {tranzit_name}: Без ошибок'
     else:
         message_for_send = 'Ошибка запуска синхронизации: \n' + sync_error
 
