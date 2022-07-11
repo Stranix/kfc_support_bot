7b1e10eec9e3476db819472e85140e4050682519
Константин <k.smurov@ucs-service.ru>
2022-07-11 15:37:47 +0300

    Возврат транзитов ИРБ

--- keyboards/choice_tr.py
+++ keyboards/choice_tr.py
@@ -1,17 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-choice_tr = InlineKeyboardMarkup(row_width=2,
+choice_tr = InlineKeyboardMarkup(row_width = 3,
                                  inline_keyboard=[
                                      [
                                        InlineKeyboardButton(text='YUM!', callback_data='yum'),
-                                     ],
-                                     [
+                                       InlineKeyboardButton(text='IRB', callback_data='irb'),
                                        InlineKeyboardButton(text='FZ', callback_data='fz'),
                                      ],
                                      [
                                        InlineKeyboardButton(text='Все', callback_data='all'),
-                                     ],
-                                     [
                                        InlineKeyboardButton(text='Отмена', callback_data='cancel')
-                                     ]
+                                     ],
                                  ])
