5c35d2102c0ed845948232eb4e6c4d6e7c2686eb
Константин <k.smurov@ucs-service.ru>
2022-07-13 19:33:47 +0300

    айдент бота

--- loader.py
+++ loader.py
@@ -12,6 +12,6 @@ FORMAT = '%(asctime)s %(levelname)s : %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p', filename='bot.log')
 logger = logging.getLogger(__name__)
 logger.info(config.BOT_TOKEN)
-bot = Bot(token='1491784267:AAHWMclXAAIm35FYbQEYRno4RdNaQLhG4UI', parse_mode=types.ParseMode.HTML)
+bot = Bot(token='437843962:AAEuSAFw6cwDn8B75yS6n1v7kXWeXJiU7D4', parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
