d26e59af93bcf0530744b5c73efcb0c3069520c3
Stranix <Phantom2525@gmail.com>
2023-04-12 16:32:44 +0300

    ADD scan_chat and find email

--- services/check_mail.py
+++ services/check_mail.py
@@ -4,14 +4,20 @@ import logging
 
 import env_config as config
 
+logger = logging.getLogger(__name__)
+
 
 def get_conn():
-    login = config.MAIL_LOGIN
-    password = config.MAIL_PASSWORD
-    logging.info('Подключаюсь к почте')
-    conn = imaplib.IMAP4_SSL('imap.yandex.ru', port=993)
-    conn.login(login, password)
-    return conn
+    try:
+        login = config.MAIL_LOGIN
+        password = config.MAIL_PASSWORD
+        imap_server = config.MAIL_IMAP_SERVER
+        logger.info('Подключаюсь к почте')
+        conn = imaplib.IMAP4_SSL(imap_server, port=993)
+        conn.login(login, password)
+        return conn
+    except imaplib.IMAP4.error as err:
+        logger.error('Ошибка соединения с почтой: %s', err)
 
 
 def get_body(msg):
@@ -31,38 +37,40 @@ def get_emails(result_bytes, conn):
 
 def find_ticket(ticket_number):
     result = False
-    logging.info(f'Ищу письмо с заявкой: {ticket_number}')
+    logger.info(f'Ищу письмо с заявкой: {ticket_number}')
     mail_conn = get_conn()
     mail_conn.select("GSD|Disp")
-    status, search_data = mail_conn.uid('search', f'(SUBJECT "{ticket_number}")')
-    logging.info(f'Status: {status}')
-    msgs = get_emails(search_data, mail_conn)
-    logging.info(f'Нашел {len(msgs)} письмо')
-    if len(msgs) > 0:
-        for msg in msgs:
-            msg_byte = get_body(email.message_from_bytes(msg[0][1]))
-            message = msg_byte.decode('UTF-8').split('---------------------------------------------------')
-            result = message[1]
-    logging.info(f'Закрытие соединения с ящиком: {mail_conn.logout()}')
+    status, search_data = mail_conn.uid('search',
+                                        f'(SUBJECT "{ticket_number}")')
+    if status == 'OK':
+        msgs = get_emails(search_data, mail_conn)
+        logger.info(f'Нашел {len(msgs)} письмо')
+        if len(msgs) > 0:
+            for msg in msgs:
+                msg_byte = get_body(email.message_from_bytes(msg[0][1]))
+                message = msg_byte.decode('UTF-8').split(
+                    '---------------------------------------------------')
+                result = message[1]
+    logger.info(f'Закрытие соединения с ящиком: {mail_conn.logout()}')
     return result
 
 
 def check_unread_mail():
     message_list = []
     mail_conn = get_conn()
-    logging.info('Перехожу в папку GSD|Disp')
+    logger.info('Перехожу в папку GSD|Disp')
     mail_conn.select("GSD|Disp")
-    logging.info('Получаю непрочитанные письма')
+    logger.info('Получаю непрочитанные письма')
     status, search_data = mail_conn.uid('search', 'UNSEEN')
-    logging.debug(f'{search_data}')
+    logger.debug(f'{search_data}')
     msgs = get_emails(search_data, mail_conn)
-    logging.info(f'Нашел {len(msgs)} писем')
+    logger.info(f'Нашел {len(msgs)} писем')
     if len(msgs) > 0:
         for msg in msgs:
             msg_byte = get_body(email.message_from_bytes(msg[0][1]))
-            message = msg_byte.decode('UTF-8').split('---------------------------------------------------')
+            message = msg_byte.decode('UTF-8').split(
+                '---------------------------------------------------')
             message_list.append(message[0] + message[1])
 
-    logging.info('Закрываю соединение с почтой')
-    logging.info(f'Закрытие соединения с ящиком: {mail_conn.logout()}')
+    logger.info(f'Закрытие соединения с ящиком: {mail_conn.logout()}')
     return message_list
