4b0687a62e4a4024b924de943728574c5c95551a
Stranix <Phantom2525@gmail.com>
2023-08-29 17:45:14 +0300

    Добавил модуль с клавиатурами для бота.
    - Удалил не нужные файлы.

---
+++ src/keyboards.py
@@ -0,0 +1,37 @@
+import logging
+
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton
+
+logger = logging.getLogger('support_bot')
+
+
+def get_choice_tr_keyboard():
+    logger.debug('Создаю клавиатуру выбора транзитов для синхронизации')
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text='YUM!', callback_data='yum'),
+            InlineKeyboardButton(text='IRB', callback_data='irb'),
+            InlineKeyboardButton(text='FZ', callback_data='fz'),
+        ],
+        [
+            InlineKeyboardButton(text='Все', callback_data='all'),
+            InlineKeyboardButton(text='Отмена', callback_data='cancel')
+        ],
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+
+def get_choice_rest_owner_keyboard():
+    logger.debug('Создаю клавиатуру для выбора группы ресторанов для синхры')
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text='YUM!',callback_data='rest_yum'),
+            InlineKeyboardButton(text='IRB', callback_data='rest_irb'),
+            InlineKeyboardButton(text='Все', callback_data='rest_all'),
+        ],
+        [
+            InlineKeyboardButton(text='Отмена', callback_data='cancel')
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
