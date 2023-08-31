91509af8ab2089586d2bd750a8d75d1fcd042d5e
Stranix <Phantom2525@gmail.com>
2023-08-31 14:36:51 +0300

    Добавил новую настройку transits.

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,9 @@
-import logging
+import os
 import sys
+import json
+import logging
+
+from pathlib import Path
 
 from pydantic_core import ValidationError
 from pydantic_settings import BaseSettings
@@ -21,6 +25,8 @@ class Settings(BaseSettings):
     mail_password: str = ''
     mail_imap_server: str = ''
 
+    transits: dict = {}
+
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8'
@@ -29,6 +35,11 @@ class Settings(BaseSettings):
 
 try:
     settings = Settings()
+    tr_settings_json = os.path.join('./', 'config/tr_settings.json')
+    settings.transits = json.loads(Path(tr_settings_json).read_text('UTF-8'))
+except FileNotFoundError:
+    logger.critical('В папке config отсутствует файл tr_settings.json')
+    sys.exit()
 except ValidationError as exc:
     logger.critical('Заданы не все обязательные настройки')
     logger.critical(exc.json())
