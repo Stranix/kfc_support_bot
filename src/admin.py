14e3d686453a418b684d6765a60d1e428ed04f4f
Stranix <Phantom2525@gmail.com>
2023-09-01 15:06:31 +0300

    Первичная регистрация моделей в админ панели.

--- src/admin.py
+++ src/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.models import (
+    Employee,
+    Task,
+    Group,
+    Right,
+    Server,
+    ServerType,
+    TaskComment,
+    Restaurant,
+    FranchiseOwner
+)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Right)
+class RightAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ServerType)
+class ServerTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskComment)
+class TaskCommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FranchiseOwner)
+class FranchiseOwner(admin.ModelAdmin):
+    pass
