5ebd1e0c8a9d35ee91031bc7c18111bc8740266f
Stranix <Phantom2525@gmail.com>
2023-10-03 15:26:11 +0300

    Добавил проверку на открытую смену в on_shift.py

--- src/bot/handlers/support/on_shift.py
+++ src/bot/handlers/support/on_shift.py
@@ -20,6 +20,9 @@ async def on_shift(message: types.Message):
     logger.info('Сотрудник заступает на смену')
     employee = await Employee.objects.prefetch_related('managers')\
                                      .aget(tg_id=message.from_user.id)
+    if await is_active_shift(employee):
+        await message.answer('У вас уже есть открытая смена')
+        return
     logger.debug('Фиксируем старт смены в БД')
     await WorkShift.objects.acreate(
         employee=employee,
@@ -35,3 +38,17 @@ async def on_shift(message: types.Message):
                 '\nЗаступил на смену',
             ),
         )
+
+
+async def is_active_shift(employee: Employee) -> bool:
+    logger.info('Проверяем есть ли у пользователя не завершенные смены')
+    try:
+        await employee.work_shifts.aget(
+            shift_start_at__isnull=False,
+            shift_end_at__isnull=True,
+        )
+        logger.info('У пользователя есть активная смена')
+        return True
+    except WorkShift.DoesNotExist:
+        logger.warning('У пользователя нет активной смены')
+        return False
