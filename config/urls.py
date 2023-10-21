80ab5ad47190bab6f3eb44f1c932e9efa9780a3a
Stranix <Phantom2525@gmail.com>
2023-10-21 23:52:35 +0300

    Добавил веб страницу с отображением смены за текущий день.

--- config/urls.py
+++ config/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('employees/', views.show_employee, name='emp'),
     path('support_tasks/', views.show_support_tasks, name='support_tasks'),
+    path('work_shifts/', views.show_shifts, name='shifts'),
 ]
--- src/views.py
+++ src/views.py
@@ -5,12 +5,14 @@ from datetime import time
 from datetime import datetime
 from datetime import timedelta
 
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
 from src.models import Task
 from src.models import Employee
+from src.models import WorkShift
 
 logger = logging.getLogger('support_web')
 
@@ -62,7 +64,7 @@ def show_support_tasks(request):
         start_at__lte=shift_end_at,
         start_at__gte=shift_start_at,
     )
-    tasks_tables = {
+    tasks_table = {
         'headers': [
             'Номер',
             'Заявитель',
@@ -79,7 +81,44 @@ def show_support_tasks(request):
         request,
         template_name='pages/sd_tasks.html',
         context={
-            'tasks_tables': tasks_tables,
+            'tasks_table': tasks_table,
             'shift_date': shift_date,
+        },
+    )
+
+
+@login_required(login_url='/accounts/login/')
+def show_shifts(request):
+    shift_date = timezone.now().date()
+    logger.debug('shift_date: %s', shift_date)
+
+    today = shift_date
+    tomorrow = today + timedelta(1)
+    shift_start_at = datetime.combine(today, time(), tzinfo=pytz.UTC)
+    shift_end_at = datetime.combine(tomorrow, time(), tzinfo=pytz.UTC)
+    shift_end_at -= timedelta(seconds=1)
+
+    works_shifts = WorkShift.objects.prefetch_related(
+        'employee',
+        'break_shift',
+    ).filter(
+        Q(shift_start_at__gte=shift_start_at),
+        Q(shift_end_at__lte=shift_end_at) | Q(shift_end_at__isnull=True),
+    )
+    shift_table = {
+        'headers': [
+            'Сотрудник',
+            'Начало смены',
+            'Перерывы',
+            'Завершение смены',
+            'Работает?',
+        ],
+        'shifts': works_shifts,
+    }
+    return render(
+        request,
+        template_name='pages/work_shifts.html',
+        context={
+            'shift_table': shift_table,
         },
     )
