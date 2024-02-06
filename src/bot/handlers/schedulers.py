289be2c27abec59e2876cab3cd763ef094839202
Stranix <Phantom2525@gmail.com>
2024-02-06 22:36:03 +0300

    FIX. Исправление задачи шедулера.
    - coroutine без await

--- src/bot/handlers/schedulers.py
+++ src/bot/handlers/schedulers.py
@@ -83,10 +83,10 @@ async def check_task_activate_step_1(task_number: str):
         return
 
     if task.support_group == 'DISPATCHER':
-        dispatcher_group = CustomGroup.objects.aget(name='Диспетчеры')
+        dispatcher_group = await CustomGroup.objects.aget(name='Диспетчеры')
         managers = dispatcher_group.managers.all()
     if task.support_group == 'ENGINEER':
-        engineer_group = CustomGroup.objects.aget(name='Инженеры')
+        engineer_group = await CustomGroup.objects.aget(name='Инженеры')
         managers = engineer_group.managers.all()
     await send_notify(managers, notify)
     await send_notify(
