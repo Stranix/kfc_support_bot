4e101a4abe0e3c168380803cdc27d62b94a3bf27
Stranix <Phantom2525@gmail.com>
2024-02-06 22:11:22 +0300

    UPDATE. Переработка обработчика старта смены
    - замена модели Employee -> CustomUser

--- src/bot/handlers/support/on_shift.py
+++ src/bot/handlers/support/on_shift.py
@@ -12,11 +12,12 @@ from aiogram.filters import Command
 
 from django.utils import timezone
 
-from src.models import Employee
+from src.models import CustomUser
 from src.models import WorkShift
-from src.bot.handlers.schedulers import check_end_of_shift
 from src.bot.utils import send_notify
+from src.bot.utils import get_employee_managers
 from src.bot.utils import send_new_tasks_notify_for_middle
+from src.bot.handlers.schedulers import check_end_of_shift
 
 logger = logging.getLogger('support_bot')
 router = Router(name='on_shift_handlers')
@@ -25,7 +26,7 @@ router = Router(name='on_shift_handlers')
 @router.message(Command('on_shift'))
 async def on_shift(
         message: types.Message,
-        employee: Employee,
+        employee: CustomUser,
         scheduler: AsyncIOScheduler,
 ):
     logger.info('Сотрудник заступает на смену')
@@ -34,7 +35,7 @@ async def on_shift(
         return
     logger.debug('Фиксируем старт смены в БД')
     shift = await WorkShift.objects.acreate(
-        employee=employee,
+        new_employee=employee,
         shift_start_at=timezone.now(),
     )
     logger.debug('Добавляю задачу на проверку окончания смены через 9 часов')
@@ -53,16 +54,17 @@ async def on_shift(
     logger.info('Отправляю уведомление менеджеру и пользователю')
     await message.answer('Вы добавлены в очередь на получение задач')
     await send_new_tasks_notify_for_middle(employee, message)
+    managers = await get_employee_managers(employee)
     await send_notify(
-        employee.managers.all(),
+        managers,
         f'Сотрудник: {html.code(employee.name)}\nЗаступил на смену',
     )
 
 
-async def is_active_shift(employee: Employee) -> bool:
+async def is_active_shift(employee: CustomUser) -> bool:
     logger.info('Проверяем есть ли у пользователя не завершенные смены')
     try:
-        await employee.work_shifts.aget(
+        await employee.new_work_shifts.aget(
             shift_start_at__isnull=False,
             shift_end_at__isnull=True,
         )
