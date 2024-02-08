c8ffd26cf46ebe1083996133b13a8d1151150768
Stranix <Phantom2525@gmail.com>
2024-02-08 16:58:34 +0300

    NEW. Заготовка на управление правами(веб)
    - Спец право для одного инженера

--- src/templatetags/auth_extras.py
+++ src/templatetags/auth_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.auth.models import Permission
 
 from src.models import CustomGroup
 
@@ -9,3 +10,11 @@ register = template.Library()
 def has_group(user, group_name):
     group = CustomGroup.objects.get(name=group_name)
     return True if group in user.groups.all() else False
+
+
+@register.filter(name='has_right')
+def has_right(user, right_name):
+    employee_permissions = Permission.objects.filter(user=user)
+    if employee_permissions.filter(codename=right_name).exists():
+        return True
+    return False
