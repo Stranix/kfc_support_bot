6242a18055b4f8d515e76181906597cc3880ca74
Stranix <Phantom2525@gmail.com>
2024-02-26 21:23:33 +0300

    NEW. Использование версионости в проекте

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -6,7 +6,7 @@ set -e
 echo -e "Выкачиваем изменения из репозитория..."
 git pull
 
-COMMIT=`git rev-parse --short HEAD`
+VERSION=`grep -m 1 version pyproject.toml | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3`
 LOCAL_USERNAME=$(whoami)
 
 echo -e "Скачиваем последний образ приложения с DockerHub"
@@ -21,6 +21,6 @@ docker compose exec web python manage.py collectstatic --no-input --clear -v 0
 echo -e "Отправляем уведомление в Rollbar..."
 curl -H "X-Rollbar-Access-Token: $ROLLBAR_POST_ACCESS_TOKEN" \
      -H "Content-Type: application/json" \
-     -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'$ROLLBAR_ENV'", "revision": "'$COMMIT'", "local_username": "'$LOCAL_USERNAME'"}'
+     -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'$ROLLBAR_ENV'", "revision": "'$VERSION'", "local_username": "'$LOCAL_USERNAME'"}'
 
 echo -e "\nВсё готово!"
